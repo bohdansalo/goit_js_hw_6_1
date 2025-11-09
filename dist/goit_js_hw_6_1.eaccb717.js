ad59c172acefc5ff342a268baefbac5cdfb5075c
Богдан <bohdansalo31@gmail.com>
2025-11-09 17:01:36 +0200

    fixed deploy

--- dist/goit_js_hw_6_1.eaccb717.js
+++
@@ -1,2 +0,0 @@
-let e,t,n;e=document.querySelector("[data-app]"),(t=document.createElement("h2")).textContent="Ukraine cyber sport team 2020",n=document.createElement("ul"),["b1t","s1mple","electroNic","Perfecto","Boombl4"].forEach(e=>{let t=document.createElement("li");t.textContent=e,n.appendChild(t)}),e.appendChild(t),e.appendChild(n);
-//# sourceMappingURL=goit_js_hw_6_1.eaccb717.js.map
